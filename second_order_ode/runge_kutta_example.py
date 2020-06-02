22e51f0907e8cbd871b6f3162cb13a1ede1a96b8
Kajetan Fricke <kajetanfricke@knights.ucf.edu>
2020-06-01 21:26:53 -0400

    runge_kutta integrator version 1

--- second_order_ode/runge_kutta_example.py
+++ second_order_ode/runge_kutta_example.py
@@ -1,99 +1,147 @@
 from tensorflow.keras.layers import RNN, Dense, Layer
 from tensorflow.keras import Sequential
-from tensorflow.keras.optimizers import RMSprop, Adam
-from tensorflow.python.framework import ops, tensor_shape
-from tensorflow.python.ops import array_ops
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.python.ops import array_ops, gen_math_ops
+from tensorflow.python.framework import ops
 import pandas as pd
 import numpy as np
-from tensorflow import float32
+import tensorflow as tf
+#from tensorflow.linalg import expm
 
 
-class EulerIntegratorCell(Layer):
-    def __init__(self, C, m, dKlayer, units=1, a0=None, **kwargs):
-        super(EulerIntegratorCell, self).__init__(**kwargs)
+class RungeKuttaIntegratorCell(Layer):
+    def __init__(self, m, c, k, dt, initial_state, input_n_dim, units=1, **kwargs):
+        super(RungeKuttaIntegratorCell, self).__init__(**kwargs)
         self.units = units
-        self.C = C
+        # self.M = tf.Variable(M, trainable=False)
+        # self.C = tf.Variable(C, trainable=True)
+        # self.K = tf.Variable(K, trainable=False)
         self.m = m
-        self.a0 = a0
-        self.dKlayer = dKlayer
-        self.state_size = tensor_shape.TensorShape(self.units)
+        self.c = c
+        self.k = k
+        self.initial_state = initial_state
+        self.state_size = 2 * input_n_dim
+        self.dt = dt
 
     def build(self, input_shape, **kwargs):
+
+        self.c_1 = tf.Variable(self.c[0], trainable=True)
+        self.c_2 = tf.Variable(self.c[1], trainable=True)
+        self.c_3 = tf.Variable(self.c[2], trainable=True)
+
+        self.m_1 = tf.Variable(self.m[0], trainable=False)
+        self.m_2 = tf.Variable(self.m[1], trainable=False)
+
+        self.k_1 = tf.Variable(self.k[0], trainable=False)
+        self.k_2 = tf.Variable(self.k[1], trainable=False)
+        self.k_3 = tf.Variable(self.k[2], trainable=False)
+
+        self.M = ops.convert_to_tensor([[self.m_1, 0], [0, self.m_2]], dtype=tf.float32)
+        self.C = ops.convert_to_tensor([[self.c_1 + self.c_2, -self.c_2], [-self.c_2, self.c_2 + self.c_3]], dtype=tf.float32)
+        self.K = ops.convert_to_tensor([[self.k_1 + self.k_2, -self.k_2], [-self.k_2, self.k_2 + self.k_3]], dtype=tf.float32)
+
+        n = self.M.shape[0]
+        self.I = tf.eye(n, dtype=tf.float32) #dtype=2self.m.dtype
+        self.Z = tf.zeros([n, n], dtype=tf.float32)
+
+        self.Minv = tf.linalg.inv(self.M)
+        self.negMinvK = -1.0 * tf.matmul(self.Minv, self.K)
+        self.negMinvC = -1.0 * tf.matmul(self.Minv, self.C)
+
+        self.A1row = tf.concat([self.Z, self.I], axis=1)
+        self.A2row = tf.concat([self.negMinvK, self.negMinvC], axis=1)
+        self.Ac = tf.concat([self.A1row, self.A2row], axis=0)
+
+        self.Bc = tf.concat([self.Z, self.Minv], axis=0)
+        self.Cc = tf.concat([self.I, self.Z], axis=1)
+        self.Dc = self.Z
+
+        self._C2D()
         self.built = True
 
-    def call(self, inputs, states):
-        inputs = ops.convert_to_tensor(inputs)
-        a_tm1 = ops.convert_to_tensor(states)
-        x_d_tm1 = array_ops.concat((inputs, a_tm1[0, :]), axis=1)
-        dk_t = self.dKlayer(x_d_tm1)
-        da_t = self.C * (dk_t ** self.m)
-        a = da_t + a_tm1[0, :]
-        return a, [a]
+    def call(self, inputs, states, training=None):
+
+        if training is not None:
+            self._C2D()
+
+        u = inputs
+        x = states[0]
+
+        y = gen_math_ops.mat_mul(x, self.Cd, transpose_b=True) + gen_math_ops.mat_mul(u, self.Dd, transpose_b=True)
+        x = gen_math_ops.mat_mul(x, self.Ad, transpose_b=True) + gen_math_ops.mat_mul(u, self.Bd, transpose_b=True)
+
+        return y, [x]
+
+    def _C2D(self):
+
+        self.negMinvC = -1.0 * tf.matmul(self.Minv, self.C)
+
+        self.A1row = tf.concat([self.Z, self.I], axis=1)
+        self.A2row = tf.concat([self.negMinvK, self.negMinvC], axis=1)
+        self.Ac = tf.concat([self.A1row, self.A2row], axis=0)
+
+        # Build an exponential matrix
+        em_upper = array_ops.concat((self.Ac, self.Bc), axis=1)
+        em_lower = array_ops.concat((tf.zeros((self.Bc.shape[1], self.Ac.shape[0]), dtype=self.Ac.dtype),
+                                     tf.zeros((self.Bc.shape[1], self.Bc.shape[1]), dtype=self.Ac.dtype)), axis=1)
+        em = array_ops.concat((em_upper, em_lower), axis=0)
+        ms = tf.linalg.expm(self.dt * em)
+        ms = ms[:self.Ac.shape[0], :]  # dispose of the lower rows
+
+        self.Ad = ms[:, 0:self.Ac.shape[1]]
+        self.Bd = ms[:, self.Ac.shape[1]:]
+
+        self.Cd = self.Cc
+        self.Dd = self.Dc
 
     def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
-        return self.a0
+        return self.initial_state
 
 
-class Normalization(Layer):
-    def __init__(self, S_low, S_up, a_low, a_up, **kwargs):
-        super(Normalization, self).__init__(**kwargs)
-        self.low_bound_S = S_low
-        self.upper_bound_S = S_up
-        self.low_bound_a = a_low
-        self.upper_bound_a = a_up
+def create_model(m, c, k, dt, batch_input_shape, initial_state = None, return_sequences = True,
+                         unroll = False):
 
-    def build(self, input_shape, **kwargs):
-        self.built = True
+    input_n_dim = batch_input_shape[-1]
+    rkCell = RungeKuttaIntegratorCell(m=m, c=c, k=k, dt=dt, initial_state=initial_state, input_n_dim=input_n_dim)
 
-    def call(self, inputs):
-        delta_x = inputs - [self.low_bound_S, self.low_bound_a]
-        output = delta_x / [(self.upper_bound_S - self.low_bound_S), (self.upper_bound_a - self.low_bound_a)]
-        return output
+    ssRNN = RNN(cell=rkCell,
+                batch_input_shape=batch_input_shape,
+                return_sequences=True,
+                return_state=False,
+                unroll=False)
 
-
-def create_model(C, m, dKlayer, a0, batch_input_shape, return_sequences):
-    euler = EulerIntegratorCell(batch_input_shape=batch_input_shape,
-                                C=C, m=m, dKlayer=dKlayer,
-                                a0=a0)
-    PINN = RNN(cell=euler, batch_input_shape=batch_input_shape, return_sequences=return_sequences,
-               return_state=False)
     model = Sequential()
-    model.add(PINN)
-    model.compile(loss='mse', optimizer=RMSprop(1e-2))
+    model.add(ssRNN)
+    model.compile(loss='mse', optimizer=RMSprop(1e0), metrics=['mae'])
 
     return model
 
-
 if __name__ == "__main__":
-    # Paris law coefficients
-    C = 1.5E-11
-    m = 3.8
-    
+    # mass, spring coefficient, damping coefficient
+    m = np.array([20.0, 10.0])
+    c = np.array([10.0, 10.0, 10.0])
+    k = np.array([2e3, 1e3, 5e3])
+
+    n = 2
+    dt = 0.002
+
     # data
-    Strain = np.asarray(pd.read_csv('Strain.csv'))[:, :, np.newaxis]
-    Stest  = np.asarray(pd.read_csv('Stest.csv'))[:, :, np.newaxis]
-    atrain = np.asarray(pd.read_csv('atrain.csv'))
-    a0 = np.asarray(pd.read_csv('a0.csv'))
-    a0 = ops.convert_to_tensor(a0, dtype=float32)
+    df = pd.read_csv('data.csv')
+    utrain = df[['u0','u1']].values
+    utest  = df[['u0','u1']].values
+    ytrain = df[['yT0','yT1']].values
 
-    # stress-intensity layer
-    dKlayer = Sequential()
-    dKlayer.add(Normalization(np.min(Strain), np.max(Strain), np.min(atrain), np.max(atrain)))
-    dKlayer.add(Dense(5, activation='tanh'))
-    dKlayer.add(Dense(1))
+    x0 = np.zeros((2 * n,), dtype='float32')
 
-    # weight initialization
-    S_range  = np.linspace(np.min(Strain), np.max(Strain), 1000)
-    a_range  = np.linspace(np.min(atrain), np.max(atrain), 1000)
-    dK_range = np.linspace(0, 40.0, 1000)
+    utrain = utrain[np.newaxis, :, :]
+    ytrain = ytrain[np.newaxis, :, :]
 
-    dKlayer.compile(loss='mse', optimizer=Adam(5e-1))
-    inputs_train = np.transpose(np.asarray([S_range, a_range]))
-    dKlayer.fit(inputs_train, dK_range, epochs=5)
+    batch_input_shape = utrain.shape
+
+    initial_state = [x0[np.newaxis, :]]
 
     # fitting physics-informed neural network
-    model = create_model(C, m, dKlayer, a0, batch_input_shape=Strain.shape, return_sequences=False)
-    model.fit(Strain, atrain, epochs=30, steps_per_epoch=1, verbose=1)
-    model_predict = create_model(C, m, dKlayer, a0, batch_input_shape=Stest.shape, return_sequences=True)
-    model_predict.set_weights(model.get_weights())
-    aPred = model_predict.predict_on_batch(Stest)[:, :, 0]
+    model = create_model(m, c, k, dt, batch_input_shape=utrain.shape, initial_state=initial_state, return_sequences=True,
+                         unroll=False)
+    model.fit(utrain, ytrain, epochs=100, steps_per_epoch=1, verbose=1)
+    yPred = model.predict_on_batch(utest)[0, :, :]
