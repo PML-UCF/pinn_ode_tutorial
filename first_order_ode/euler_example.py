fd3de23f84cf1972526eb76a82b1dac6c914c7c0
Felipe Viana <44379653+felipeacviana@users.noreply.github.com>
2020-06-03 19:01:00 -0400

    Update euler_example.py

--- first_order_ode/euler_example.py
+++ first_order_ode/euler_example.py
@@ -12,23 +12,23 @@ class EulerIntegratorCell(Layer):
     def __init__(self, C, m, dKlayer, a0=None, units=1, **kwargs):
         super(EulerIntegratorCell, self).__init__(**kwargs)
         self.units = units
-        self.C = C
-        self.m = m
-        self.a0 = a0
-        self.dKlayer = dKlayer
-        self.state_size = tensor_shape.TensorShape(self.units)
+        self.C     = C
+        self.m     = m
+        self.a0    = a0
+        self.dKlayer     = dKlayer
+        self.state_size  = tensor_shape.TensorShape(self.units)
         self.output_size = tensor_shape.TensorShape(self.units)
 
     def build(self, input_shape, **kwargs):
         self.built = True
 
     def call(self, inputs, states):
-        inputs = ops.convert_to_tensor(inputs)
-        a_tm1 = ops.convert_to_tensor(states)
+        inputs  = ops.convert_to_tensor(inputs)
+        a_tm1   = ops.convert_to_tensor(states)
         x_d_tm1 = array_ops.concat((inputs, a_tm1[0, :]), axis=1)
-        dk_t = self.dKlayer(x_d_tm1)
-        da_t = self.C * (dk_t ** self.m)
-        a = da_t + a_tm1[0, :]
+        dk_t    = self.dKlayer(x_d_tm1)
+        da_t    = self.C * (dk_t ** self.m)
+        a       = da_t + a_tm1[0, :]
         return a, [a]
 
     def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
@@ -38,17 +38,16 @@ class EulerIntegratorCell(Layer):
 class Normalization(Layer):
     def __init__(self, S_low, S_up, a_low, a_up, **kwargs):
         super(Normalization, self).__init__(**kwargs)
-        self.low_bound_S = S_low
+        self.low_bound_S   = S_low
         self.upper_bound_S = S_up
-        self.low_bound_a = a_low
+        self.low_bound_a   = a_low
         self.upper_bound_a = a_up
 
     def build(self, input_shape, **kwargs):
         self.built = True
 
     def call(self, inputs):
-        delta_x = inputs - [self.low_bound_S, self.low_bound_a]
-        output = delta_x / [(self.upper_bound_S - self.low_bound_S), (self.upper_bound_a - self.low_bound_a)]
+        output  = (inputs - [self.low_bound_S, self.low_bound_a]) / [(self.upper_bound_S - self.low_bound_S), (self.upper_bound_a - self.low_bound_a)]
         return output
 
 
@@ -79,8 +78,8 @@ if __name__ == "__main__":
     dKlayer.add(Dense(1))
 
     # weight initialization
-    S_range = np.linspace(np.min(Strain), np.max(Strain), 1000)
-    a_range = np.linspace(np.min(atrain), np.max(atrain), 1000)[np.random.permutation(np.arange(1000))]
+    S_range  = np.linspace(np.min(Strain), np.max(Strain), 1000)
+    a_range  = np.linspace(np.min(atrain), np.max(atrain), 1000)[np.random.permutation(np.arange(1000))]
     dK_range = -12.05 + 0.24 * S_range + 760.0 * a_range
 
     dKlayer.compile(loss='mse', optimizer=RMSprop(1e-1))
