532c70aae5005cb4881563ea904f3fac4b176cda
Augusto <augustod.marques@gmail.com>
2022-06-02 11:52:39 -0300

    Update PyTorch example

--- first_order_ode/euler_example_PyTorch.py
+++ first_order_ode/euler_example_PyTorch.py
@@ -4,161 +4,213 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as matplotlib
 
-from torch import nn, Tensor, sqrt, pi, stack, optim
+from torch import (
+    nn,
+    Tensor,
+    sqrt,
+    pi,
+    stack,
+    cat,
+    randperm,
+    min, 
+    max,
+    linspace,
+    transpose, 
+    optim
+    )
 
 
 class Normalization(nn.Module):
     def __init__(self, S_low, S_up, a_low, a_up, **kwargs):
         super(Normalization, self).__init__(**kwargs)
-        self.low_bound_S   = S_low
+        self.low_bound_S = S_low
         self.upper_bound_S = S_up
-        self.low_bound_a   = a_low
+        self.low_bound_a = a_low
         self.upper_bound_a = a_up
 
-    def forward(self, x):
-        
-        s = x[:,0]
-        a = x[:,1]
-        
-        s  = (s - self.low_bound_S) / (self.upper_bound_S - self.low_bound_S)
-        a  = (a - self.low_bound_a) / (self.upper_bound_a - self.low_bound_a)
-        
-        return stack((s,a)).T
+    def forward(self, inputs):
+
+        output_S = (inputs[:, 0] - self.low_bound_S)/(self.upper_bound_S - self.low_bound_S)
+        output_a = (inputs[:, 1] - self.low_bound_a)/(self.upper_bound_a - self.low_bound_a)
+
+        return stack([output_S,output_a], dim=1)
 
 
-class EulerIntegrator(nn.Module):
-    def __init__(self, C, m, F, **kwargs):
-        super(EulerIntegrator, self).__init__(**kwargs)
-        self.C = C
-        self.m = m
-        self.F = F
-    
-    def forward(self, x, a0):
-        
-        # x is a tensor of shape (sequence_length, input_size)
-        (sequence_length, input_size) = x.shape
-        
-        a_t  = a0
-        a    = a0.repeat(sequence_length,1)
-        for t in range(sequence_length):
-            x_t  = x[t, :]
-            dk_t = self.F * x_t * sqrt(pi * a_t)
-            da_t = self.C * (dk_t ** self.m)
-            a_t  = a_t + da_t
-            a[t] = a_t
-        
+class MyRNN(nn.Module):
+    def __init__(self, cell):
+        super(MyRNN, self).__init__()
+        self.cell = cell
+
+    def forward(self, inputs, a0):
+
+        bs, seq_sz, _ = inputs.shape
+        a = a0
+        for t in range(seq_sz): 
+            input = inputs[:, t, :]
+            state = self.cell.forward(input, a) 
+            a = a+state
+
         return a
 
 
-class HybridEulerIntegrator(nn.Module):
+class EulerIntegratorCell(nn.Module):
     def __init__(self, C, m, dKlayer, **kwargs):
-        super(HybridEulerIntegrator, self).__init__(**kwargs)
+        super(EulerIntegratorCell, self).__init__(**kwargs)
         self.C = C
         self.m = m
         self.dKlayer = dKlayer
-    
-    def forward(self, x, a0):
+
+    def forward(self, inputs, states): 
+
+        a_tm1 = states
+        x_d_tm1 = cat((inputs, a_tm1), dim=1)
+        dk_t = self.dKlayer(x_d_tm1)
+        da_t = self.C * (dk_t ** self.m)
+
+        return da_t
+
+
+class DkPhys(nn.Module):
+    def __init__(self, F, **kwargs):
+        super(DkPhys, self).__init__(**kwargs)
+        self.F = F
+
+    def forward(self, x_d):
+
+        dk_t = self.F * x_d[:,0] * sqrt(pi * x_d[:,1])
+        dk_t = dk_t[:,None]
+
+        return dk_t
+
+
+class DkNN(nn.Module):
+    def __init__(self,Strain, atrain, **kwargs):
+        super(DkNN, self).__init__(**kwargs)
+        # stress-intensity layer
+        self.dKlayer = nn.Sequential(
+        Normalization(min(Strain), max(Strain), min(atrain), max(atrain)),
+        nn.Linear(2, 5),
+        nn.Tanh(),
+        nn.Linear(5, 1)
+        )
+
+    def forward(self, x):
+
+        dk_t = self.dKlayer(x)
+
+        return dk_t
+
+
+def dk_training_loop(n_epochs,batch_size, optimizer, model, loss_fn, train, label):
+    for epoch in range(1, n_epochs + 1):
         
-        # x is a tensor of shape (sequence_length, input_size)
-        (sequence_length, input_size) = x.shape
-        
-        a_t  = a0
-        a    = a0.repeat(sequence_length,1)
-        for t in range(sequence_length):
-            x_t  = x[t, :]
-            dk_t = self.dKlayer(stack((x_t, a_t),1))[:,0]
-            da_t = self.C * (dk_t ** self.m)
-            a_t  = a_t + da_t
-            a[t] = a_t
-        
-        return a
-        
-        
+        permutation = randperm(train.shape[0])
+        for i in range(0,train.shape[0], batch_size):
+
+            #Minibatch
+            indices = permutation[i:i+batch_size]
+            batch_x, batch_y = train[indices], label[indices]
+            
+            #Forward pass
+            output_train = model(batch_x)
+            loss_train = loss_fn(output_train, batch_y)
+
+            #Backward pass
+            optimizer.zero_grad()
+            loss_train.backward()
+            optimizer.step()
+
+        print(f"{int(i/batch_size)+1}/{batch_size}-Epoch {epoch}, Training loss {loss_train.item():.4e}")
+
+def hPINN_training_loop(n_epochs, optimizer, model, loss_fn, train, a0, label):
+    for epoch in range(1, n_epochs + 1):        
+            #Forward pass
+            output_train = model(train, a0)
+            loss_train = loss_fn(output_train, label)
+
+            #Backward pass
+            optimizer.zero_grad()
+            loss_train.backward()
+            optimizer.step()
+            
+            print(f"Epoch {epoch}, Training loss {loss_train.item():.4e}")
+
+
 if __name__ == "__main__":
     # Paris law coefficients
     [C, m] = [1.5E-11, 3.8]
     F = 1.0
     
     # data
-    Strain = np.asarray(pd.read_csv('./data/Strain.csv')).T
+    Strain = np.asarray(pd.read_csv('first_order_ode/data/Strain.csv'))[:,:,np.newaxis]
     Strain = Tensor(Strain)
-    
-    atrain = np.asarray(pd.read_csv('./data/atrain.csv')).T
+    atrain = np.asarray(pd.read_csv('first_order_ode/data/atrain.csv'))
     atrain = Tensor(atrain)
-    
-    a0 = np.asarray(pd.read_csv('./data/a0.csv'))[0,0]
-    a0 = Tensor(a0*np.ones((Strain.shape[1])))
-    
-    PINN  = EulerIntegrator(C=C, m=m, F=F)
-    aPINN = PINN(Strain, a0)
-    
+    a0 = np.asarray(pd.read_csv('first_order_ode/data/a0.csv'))[0,0]*np.ones((Strain.shape[0],1))
+    a0 = Tensor(a0)
+
     "-------------------------------------------------------------------------"
-    " dKlayer and weight initialization "
-    dKlayer = nn.Sequential(Normalization(Strain.min(), Strain.max(), atrain.min(), atrain.max()),
-                            nn.Linear(2, 5),
-                            nn.Tanh(),
-                            nn.Linear(5, 1))
+    "PINN with DkPhys "
+    dk_phys = DkPhys(F)
+    euler = EulerIntegratorCell(C=C, m=m, dKlayer=dk_phys)
+    model_PINN = MyRNN(cell = euler)
+    aPred = model_PINN(Strain, a0)[:, :]
 
-    S_range  = np.linspace(Strain.min(), Strain.max(), 1000)
-    a_range  = np.linspace(atrain.min(), atrain.max(), 1000)[np.random.permutation(np.arange(1000))]
-    dK_range = Tensor(np.array(-12.05 + 0.24 * S_range + 760.0 * a_range)[:,np.newaxis])
-    inputs_train = Tensor(np.transpose(np.asarray([S_range, a_range])))
+    "-------------------------------------------------------------------------"
+    " dKlayerNN and weight initialization "
+    S_range = linspace(min(Strain), max(Strain), 1000)
+    a_range = linspace(min(atrain), max(atrain), 1000)[randperm(1000)]
+    dK_range = -12.05 + 0.24 * S_range + 760.0 * a_range
+    dK_range = dK_range [:,None]
 
-    loss_fn = nn.MSELoss()
-    learning_rate = 1e-2
-    optimizer = optim.RMSprop(dKlayer.parameters(), lr=learning_rate)
-    
-    epochs = 2000
-    dKlayer.eval()
-    dKlayer.train()
-    for t in range(epochs):
-        optimizer.zero_grad()
-        Ypred = dKlayer(inputs_train)
-        loss = loss_fn(Ypred, dK_range)
-        loss.backward()
-        optimizer.step()
-        
-        print(t, loss.item())
-            
+    dKlayer = DkNN(Strain,atrain)
+
+    dk_training_loop(
+        n_epochs = 100,
+        batch_size = 32,
+        optimizer = optim.RMSprop(dKlayer.parameters(), lr=1e-2),
+        model = dKlayer,
+        loss_fn = nn.MSELoss(),
+        train = transpose(stack([S_range,a_range]),0,1),
+        label = dK_range
+        )
+
     "-------------------------------------------------------------------------"
     " hPINN training "
-    hPINN  = HybridEulerIntegrator(C=C, m=m, dKlayer=dKlayer)
-    aPred_before = hPINN(Strain, a0)
+    h_euler = EulerIntegratorCell(C=C, m=m, dKlayer=dKlayer)
+    model_hPINN = MyRNN(cell = h_euler)
+    aPred_before = model_hPINN(Strain, a0)[:,:]
 
-    loss_fn = nn.MSELoss()
-    learning_rate = 1e-2
-    optimizer = optim.RMSprop(hPINN.parameters(), lr=learning_rate)
-    
-    epochs = 100
-    hPINN.eval()
-    hPINN.train()
-    for t in range(epochs):
-        optimizer.zero_grad()
-        y_pred = hPINN(Strain, a0)
-        loss = loss_fn(y_pred[-1,:], atrain[0,:])
-        loss.backward()
-        optimizer.step()
-        print(t, loss.item())
+    hPINN_training_loop(
+        n_epochs = 100,
+        optimizer = optim.RMSprop(model_hPINN.parameters(), lr=1e-2),
+        model = model_hPINN,
+        loss_fn = nn.MSELoss(),
+        train = Strain,
+        a0 = a0,
+        label = atrain,
+    )
 
+    h_aPred = model_PINN(Strain, a0)[:,:]
 
-    aPred = hPINN(Strain, a0)
-    
     "-------------------------------------------------------------------------"
     " plottting "
     matplotlib.rc('font', size=12)
-    ifig = 0
-    
-    ifig = ifig + 1
-    fig  = plt.figure(ifig)
-    fig.clf()
 
-    plt.plot([0,0.05],[0,0.05],'--k')
-    plt.plot(atrain[0,:], aPred_before.detach()[-1,:], 'o', label = 'before training')
-    plt.plot(atrain[0,:], aPred.detach()[-1,:], 's', label = 'after training')
-    plt.xlabel("actual crack length (m)")
-    plt.ylabel("predicted crack length (m)")
-    plt.title("PyTorch")
-    plt.legend(loc = 'upper center',facecolor = 'w')
-    plt.grid(which='both')
+    fig, ax= plt.subplots(1,2,sharey=True, figsize=(2*6.4,4.8),gridspec_kw={'hspace': 0, 'wspace': 0.1}) 
+
+    ax[0].plot([0,0.05],[0,0.05],'--k')
+    ax[0].plot(atrain.detach().numpy(), aPred_before.detach().numpy(), 'o', label = 'before training')
+    ax[0].plot(atrain.detach().numpy(), h_aPred.detach().numpy(), 's', label = 'after training')
+    ax[0].set_xlabel("actual crack length (m)")
+    ax[0].set_ylabel("predicted crack length (m)")
+    ax[0].legend(loc = 'upper left',facecolor = 'w')
+    ax[0].grid(which='both')
+
+    ax[1].plot([0,0.05],[0,0.05],'--k')
+    ax[1].plot(atrain.detach().numpy(), aPred.detach().numpy(), 's', label = 'physical $\Delta$K')
+    ax[1].grid(which='both')
+    ax[1].set_xlabel("actual crack length (m)")
+    ax[1].legend(loc = 'upper left',facecolor = 'w')
+
     plt.show()
-    
